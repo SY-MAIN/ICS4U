ba6d7dfe2cac194fdd17700bd17683e8cc260683
SY-MAIN <xxsimonxx119@gmail.com>
2021-10-31 12:39:57 -0400

    file loading

--- Raft/Main.java
+++ Raft/Main.java
@@ -1,7 +1,16 @@
-import java.util.Scanner;
+import java.io.File;
 
 class Main {
   public static void main(String[] args) {
+
+    File ArtDir = new File("./ASCII_ART");
+
+    String fileName = "Idle";
+
+    File newFile = new File(ArtDir, fileName);
+    System.out.println(newFile.exists());
+
+    System.out.println(FileLoader.readFile(newFile));
 
   }
 }
