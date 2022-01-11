51c05d442491aeec606370da579bf3ee2dfed600
SY-MAIN <xxsimonxx119@gmail.com>
2022-01-10 22:47:39 -0500

    resolve changes

---
+++ City/src/Sketch.java
@@ -0,0 +1,18 @@
+
+import processing.core.*;
+
+public class Sketch extends PApplet {
+
+  public void settings() {
+    size(400, 400);
+  }
+
+  public void setup() {
+    background(0);
+  }
+
+  public void draw() {
+    // fill(255);
+    ellipse(mouseX, mouseY, 25, 25);
+  }
+}
